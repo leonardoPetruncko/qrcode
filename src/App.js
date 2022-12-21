5cba01e837082cf35df2051b3016b74e470c64f3
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-21 19:08:32 -0300

    Projeto

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import QRcode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
+
+import './App.css';
+
+function App() {
+  const [link, setLink] = useState('')
+  const [qrcodeLink, setQrcodeLink] = useState('')
+
+  function handleGenerate(link_url) {
+    QRCodeLink.toDataURL(link_url, {
+      width: 600,
+      margin: 3,
+    }, function (err, url){
+      setQrcodeLink(url);
+    })
+  }
+
+  function handleQrcode(e) {
+    setLink(e.target.value)
+    handleGenerate(e.target.value)
+  }
+  return (
+
+    <div className='container'>
+      <QRcode
+      value={link} 
+      />
+      
+      <input 
+      className='input' 
+      placeholder='Digite seu link'
+      value={link}
+      onChange={ (e) => handleQrcode(e)}
+      />
+
+    <a href={qrcodeLink} download={`qrcode.png`}>Baixar QRcode</a>
+
+    </div>
+
+  )
+}
+
+export default App;
