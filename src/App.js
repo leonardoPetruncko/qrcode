f6eb27ce7b3e564832aef1a5b80e233170776d1f
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-25 00:44:19 -0300

    Resolvendo erro

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
       onChange={ (e) => handleQrcode(e)}
       />
 
-    <a  download={`qrcode.png`}>Baixar QRcode</a>
+    <a href={qrcodeLink}  download={`qrcode.png`}>Baixar QRcode</a>
 
     </div>
 
